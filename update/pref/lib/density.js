6755d48578c2b718738af43785fb536b9945ad2c
仁志 <legend1world@gmail.com>
2022-01-15 15:39:31 +0900

    up

---
+++ update/pref/lib/density.js
@@ -0,0 +1,57 @@
+  var density = new Array(47);
+  
+  /* 人口 */
+			
+density[0]="5250000";
+density[1]="1246000";
+density[2]="1227000";
+density[3]="2306000";
+density[4]="966000";
+density[5]="1078000";
+density[6]="1846000";
+density[7]="2860000";
+density[8]="1934000";
+density[9]="1942000";
+density[10]="7350000";
+density[11]="6259000";
+density[12]="13921000";
+density[13]="9198000";
+density[14]="2223000";
+density[15]="1044000";
+density[16]="1138000";
+density[17]="768000";
+density[18]="811000";
+density[19]="2049000";
+density[20]="1987000";
+density[21]="3644000";
+density[22]="7552000";
+density[23]="1781000";
+density[24]="1414000";
+density[25]="2583000";
+density[26]="8809000";
+density[27]="5466000";
+density[28]="1330000";
+density[29]="925000";
+density[30]="556000";
+density[31]="674000";
+density[32]="1890000";
+density[33]="2804000";
+density[34]="1358000";
+density[35]="728000";
+density[36]="956000";
+density[37]="1339000";
+density[38]="698000";
+density[39]="5104000";
+density[40]="815000";
+density[41]="1327000";
+density[42]="1748000";
+density[43]="1135000";
+density[44]="1073000";
+density[45]="1602000";
+density[46]="1453000";
+
+function return_top(){
+
+	location.href = "../index.html";
+
+}
